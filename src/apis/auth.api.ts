c675f385490f505c419e6c926b0574ad1fe58d4a
leechan <dlcks0601@gmail.com>
2025-05-27 21:13:19 +0900

    [Add] 회원가입 기능을 위한 API 구현

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -1,4 +1,4 @@
-import { LoginResponse } from '@/types/auth.type';
+import { LoginResponse, SignupResponse } from '@/types/auth.type';
 import fetcher from '@/utils/fetcher';
 
 export const login = async (id: string, password: string) => {
@@ -8,6 +8,20 @@ export const login = async (id: string, password: string) => {
     data: {
       id,
       password,
+    },
+  });
+  console.log(response.data);
+  return response.data;
+};
+
+export const signup = async (id: string, password: string, role: string) => {
+  const response = await fetcher<SignupResponse>({
+    url: '/auth/signup',
+    method: 'POST',
+    data: {
+      id,
+      password,
+      role,
     },
   });
   console.log(response.data);
